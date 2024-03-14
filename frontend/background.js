5e4c3dbe2dc47184cb282b0ba28eed192eb1eae5
Saurabh Basak <saurabhbasak1@gmail.com>
2024-03-14 14:21:00 -0400

    Add stock button functionality

---
+++ frontend/background.js
@@ -0,0 +1,16 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+    if (changeInfo.status === "complete") {
+        chrome.tabs.get(tabId, function (tab) {
+            if (tab.url && tab.url.includes("google.com/search")) {
+                const queryParameters = tab.url.split("?")[1];
+                const urlParameters = new URLSearchParams(queryParameters);
+
+                console.log(urlParameters.get("q"));
+                chrome.tabs.sendMessage(tabId, {
+                    type: "NEW",
+                    stockId: urlParameters.get("q"),
+                });
+            }
+        });
+    }
+});
