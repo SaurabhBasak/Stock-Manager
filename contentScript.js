060c775ab7112e89376dc5bf20ac62c020a923f8
Saurabh Basak <saurabhbasak1@gmail.com>
2024-04-06 11:01:26 -0400

    Switched storage handling to service worker for better syncing

--- contentScript.js
+++ contentScript.js
@@ -1,62 +1,20 @@
 (() => {
     let topLeftPanel;
-    let newStock = "";
-    let currentStocks = [];
-
-    const fetchStocks = async () => {
-        return new Promise((resolve) => {
-            chrome.storage.sync.get([newStock], (obj) => {
-                resolve(obj[newStock] ? JSON.parse(obj[newStock]) : []);
-            });
-        });
-    };
 
     chrome.runtime.onMessage.addListener((object, sender, response) => {
-        const { type, value, stockId } = object;
-        console.log(stockId);
+        const { type, ticker } = object;
+        console.log(ticker);
 
         if (type === "NEW") {
-            newStock = stockId;
-            sendSearchParams(stockId);
-            // .then(async (ticker) => {
-            //     let tickerPrice = await fetchStockData(ticker);
-            //     console.log(tickerPrice);
-            //     chrome.runtime.sendMessage({
-            //         action: "updatePopup",
-            //         ticker: ticker,
-            //         tickerPrice: tickerPrice,
-            //     });
-            // });
+            console.log("Ticker", ticker);
+            newStockAdded(ticker);
         }
     });
-
-    async function sendSearchParams(stockId) {
-        try {
-            const response = await fetch("http://127.0.0.1:5000/searchParams", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ stockId }),
-            });
-            if (!response.ok) {
-                throw new Error("Failed to send data to backend");
-            }
-            const responseData = await response.json();
-            console.log("Response from backend:", responseData);
-            newStockAdded(responseData["ticker"]);
-            return responseData["ticker"];
-        } catch (error) {
-            console.error("Error in sending search params: ", error);
-        }
-    }
 
     const newStockAdded = async (ticker) => {
         const addStockBtnExists =
             document.getElementsByClassName("add-stock-btn")[0];
         const isStock = document.getElementsByClassName("REySof")[0];
-
-        currentStocks = await fetchStocks();
 
         if (!addStockBtnExists && isStock) {
             const addStockBtn = document.createElement("button");
@@ -65,56 +23,15 @@
             addStockBtn.title = "Add stock to track it";
             addStockBtn.innerHTML = `Add ${ticker}`;
 
-            topLeftPanel = document.getElementsByClassName("gb_Ud")[0];
-            topLeftPanel.appendChild(addStockBtn);
+            topLeftPanel = document.getElementsByClassName("gb_a gb_i gb_ld")[0];
+            const newDiv = topLeftPanel.appendChild(document.createElement("div"));
+            newDiv.appendChild(addStockBtn);
             addStockBtn.addEventListener("click", () =>
-                addStockEventHandler(ticker)
+                chrome.runtime.sendMessage({
+                    action: "addStock",
+                    ticker: ticker,
+                })
             );
         }
     };
-
-    const addStockEventHandler = async (ticker) => {
-        let tickerPrice = await fetchStockData(ticker);
-        tickerPrice = tickerPrice.toFixed(2);
-        // chrome.runtime.sendMessage({
-        //     action: "addStock",
-        //     ticker: ticker,
-        //     tickerPrice: tickerPrice,
-        // });
-        const newStockDetails = {
-            ticker,
-            tickerPrice,
-        };
-
-        currentStocks = await fetchStocks();
-
-        console.log(newStockDetails);
-        console.log(currentStocks);
-
-        if (!currentStocks.find((stock) => stock["ticker"] === newStockDetails["ticker"])){
-            chrome.storage.sync.set({
-                [newStock]: JSON.stringify([...currentStocks, newStockDetails]),
-            });
-        }
-    };
-
-    async function fetchStockData(ticker) {
-        try {
-            const response = await fetch("http://127.0.0.1:5000/stock", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ ticker: ticker }),
-            });
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            const data = await response.json();
-            return data.currentPrice;
-        } catch (error) {
-            console.error("Error fetching stock data:", error);
-            throw error; // Rethrow the error to be caught in the caller
-        }
-    }
 })();
--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,102 @@
-chrome.tabs.onUpdated.addListener( async (tabId, changeInfo) => {
+let newTicker = "";
+let currentStocks = [];
+
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
     if (changeInfo.status === "complete") {
-        chrome.tabs.get(tabId, function (tab) {
+        chrome.tabs.get(tabId, async function (tab) {
             if (tab.url && tab.url.includes("google.com/search")) {
                 const queryParameters = tab.url.split("?")[1];
                 const urlParameters = new URLSearchParams(queryParameters);
 
-                console.log(urlParameters.get("q"));
+                // console.log(urlParameters.get("q"));
+                const parameters = urlParameters.get("q");
+                const ticker = await sendSearchParams(parameters, tabId);
+
                 chrome.tabs.sendMessage(tabId, {
                     type: "NEW",
-                    stockId: urlParameters.get("q"),
+                    ticker,
                 });
             }
         });
     }
 });
+
+
+const fetchStocks = async () => {
+    return new Promise((resolve) => {
+        chrome.storage.sync.get([newTicker], (obj) => {
+            resolve(obj[newTicker] ? JSON.parse(obj[newTicker]) : []);
+        });
+    });
+};
+
+
+async function sendSearchParams(parameters, tabId) {
+    try {
+        const response = await fetch("http://127.0.0.1:5000/searchParams", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ parameters: parameters }),
+        });
+        if (!response.ok) {
+            throw new Error("Failed to send data to backend");
+        }
+        const responseData = await response.json();
+        console.log("Response from backend:", responseData);
+        return responseData["ticker"];
+    } catch (error) {
+        console.error("Error in sending search params: ", error);
+    }
+}
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    const { action, ticker } = request;
+    if (action === "addStock") {
+        addStockEventHandler(ticker);
+    }
+});
+
+const addStockEventHandler = async (ticker) => {
+    let tickerPrice = await fetchStockData(ticker);
+    tickerPrice = tickerPrice.toFixed(2);
+    const newStockDetails = {
+        ticker,
+        tickerPrice,
+    };
+
+    currentStocks = await fetchStocks();
+
+    console.log(newStockDetails);
+    console.log(currentStocks);
+
+    if (!currentStocks.find((stock) => stock["ticker"] === newStockDetails["ticker"])){
+        chrome.storage.sync.set({
+            [newTicker]: JSON.stringify([...currentStocks, newStockDetails]),
+        });
+    }
+};
+
+
+async function fetchStockData(ticker) {
+    try {
+        const response = await fetch("http://127.0.0.1:5000/stock", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ ticker: ticker }),
+        });
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        return data.currentPrice;
+    } catch (error) {
+        console.error("Error fetching stock data:", error);
+        throw error;
+    }
+}
