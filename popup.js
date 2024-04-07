8bb838f8308832aacfe930e46f36760e5615e252
Saurabh Basak <saurabhbasak1@gmail.com>
2024-04-07 13:34:28 -0400

    Refactored popup.js to display stock ticker, price, and price range

--- popup.js
+++ popup.js
@@ -2,11 +2,38 @@ function updateView() {
     chrome.storage.sync.get(["currentStocks"], (obj) => {
         const currentStocks = obj["currentStocks"] ? JSON.parse(obj["currentStocks"]) : {};
         // console.log("Popup", currentStocks);
+        // const currentStocks = {};
         const stockList = document.getElementById("stock-list");
         for (const ticker in currentStocks) {
-            const stockItem = document.createElement("div").appendChild(document.createElement("h2"));
-            stockItem.className = `${ticker} stock`;
-            stockItem.innerHTML = `${ticker}: ${currentStocks[ticker].price}`;
+            const stockItem = document.createElement("div");
+            stockItem.className = `${ticker}-stock`;
+            const stockTicker = document.createElement("h3");
+            stockItem.appendChild(stockTicker);
+            stockTicker.innerHTML = `${ticker}: ${currentStocks[ticker].price}`;
+
+            const stockPriceRange = document.createElement("div");
+
+            stockPriceRange.className = `${ticker}-price-range`;
+            const startLabel = document.createElement("label");
+            startLabel.innerHTML = "Low";
+            const startRange = document.createElement("input");
+            startRange.placeholder = "Low";
+            startRange.value = currentStocks[ticker].low;
+
+            const endLabel = document.createElement("label");
+            endLabel.innerHTML = "High";
+            const endRange = document.createElement("input");
+            endRange.placeholder = "High";
+            endRange.value = currentStocks[ticker].high;
+
+            stockPriceRange.appendChild(startLabel);
+            stockPriceRange.appendChild(startRange);
+            stockPriceRange.appendChild(endLabel);
+            stockPriceRange.appendChild(endRange);
+            stockItem.appendChild(stockPriceRange);
+            
+            stockItem.appendChild(document.createElement("br"));
+            stockItem.appendChild(document.createElement("hr"));
             stockList.appendChild(stockItem);
         }
     });
