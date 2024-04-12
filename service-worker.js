5ada80d6ad975010a330dd1a9c9aa65d9043f5f5
Oshaidah <112043011+Oshaidah@users.noreply.github.com>
2024-04-11 22:02:51 -0400

    test 2

--- service-worker.js
+++ service-worker.js
@@ -79,6 +79,55 @@ const addStockEventHandler = async (ticker) => {
     }
 };
 
+
+setInterval(async () => {
+    const currentStocks = await fetchStocks();
+
+    if (Object.keys(currentStocks).length > 0) {
+        const tickersString = Object.keys(currentStocks).join(" ");
+        let allPrices = await fetchMassStockData(tickersString); 
+        console.log(allPrices);
+        for (let ticker in currentStocks) {
+            currentStocks[ticker].price = allPrices[ticker].toFixed(2);
+            
+            if ( currentStocks[ticker].price <  currentStocks[ticker].low) {
+                console.log(
+                    `Price of ${ticker} is below the low price. Current price: ${currentStocks[ticker].price}, Low price: ${currentStocks[ticker].low}`
+                );
+            } else if ( currentStocks[ticker].price >  currentStocks[ticker].high) {
+                console.log(
+                    `Price of ${ticker} is above the high price. Current price: ${currentStocks[ticker].price}, High price: ${currentStocks[ticker].high}`
+                );
+            }
+        }
+
+        chrome.storage.sync.set({
+            currentStocks: JSON.stringify(currentStocks),
+        });
+    }
+}, 60000);
+
+async function fetchMassStockData(tickers) {
+    try {
+        const response = await fetch("http://127.0.0.1:5000/massStock", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ tickers: tickers }),
+        });
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const prices = await response.json();
+        return prices;
+    } catch (error) {
+        console.error("Error fetching stock data:", error);
+        throw error;
+    }
+}
+
+
 async function fetchStockData(ticker) {
     try {
         const response = await fetch("http://127.0.0.1:5000/stock", {
